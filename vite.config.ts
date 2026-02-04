b7d5417cb46789fd93941267b52251aafc017edf
M-Lenvik <lenvik.marie@gmail.com>
2026-02-04 12:24:37 +0100

    Configure GitHub Pages deployment with GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/Javascript-ramverk-React-techReq/' : '/',
   optimizeDeps: {
     exclude: ['@digi/arbetsformedlingen'],
   },
